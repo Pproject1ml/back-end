0b99ccd0272576c2d3ea2e54ecf002463e81a25a
1eehaewon <lhw2112@nate.com>
2025-01-10 15:56:04 +0900

    feat : 이미 존재하는 랜드마크인지 경도,위도로 확인

--- src/main/java/org/_1mg/tt_backend/landmark/LandmarkRepository.java
+++ src/main/java/org/_1mg/tt_backend/landmark/LandmarkRepository.java
@@ -4,6 +4,10 @@ import org._1mg.tt_backend.landmark.entity.Landmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LandmarkRepository extends JpaRepository<Landmark, Integer> {
+    // 위도와 경도로 랜드마크를 조회
+    Optional<Landmark> findByLatitudeAndLongitude(Double latitude, Double longitude);
 }
