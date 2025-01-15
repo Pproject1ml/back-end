bca9126ce06d7ccd10fcf0ba023cb2cca569c403
whffu762 <whffu762@naver.com>
2025-01-15 19:49:33 +0900

    refactor : 불필요 이벤트 리스터 제거 - APP 메모리 관리 방식 변경으로 인해 메세지 전송 방식 변경 - 기존 : 각 채팅방에 들어갈 때마다 구독 끊고 다시 연결하는 방식 - 현 : 앱 들어갈 때 한 번 구독 연결 후 끝날 때까지 유지

--- src/main/java/org/_1mg/tt_backend/chat/config/MessageInterceptor.java
+++ src/main/java/org/_1mg/tt_backend/chat/config/MessageInterceptor.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class MessageInterceptor implements ChannelInterceptor {
 
+    //COMMAND가 SEND 메세지 수신 시 로그
     @Override
     public Message<?> preSend(Message<?> message, MessageChannel channel) {
         StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
@@ -25,26 +26,5 @@ public class MessageInterceptor implements ChannelInterceptor {
 
 
         return message;
-    }
-
-    @Override
-    public void postSend(Message<?> message, MessageChannel channel, boolean sent) {
-        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        String sessionId = accessor.getSessionId();
-
-        switch (accessor.getCommand()) {
-
-            case CONNECT:
-                log.info("세션 들어온 -> {}", sessionId);
-                break;
-
-            case DISCONNECT:
-                log.info("세션 끊음 -> {}", sessionId);
-                break;
-
-            default:
-                break;
-        }
-        ChannelInterceptor.super.postSend(message, channel, sent);
     }
 }
--- src/main/java/org/_1mg/tt_backend/chat/config/WebSocketEventListener.java
+++ src/main/java/org/_1mg/tt_backend/chat/config/WebSocketEventListener.java
@@ -2,15 +2,10 @@ package org._1mg.tt_backend.chat.config;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org._1mg.tt_backend.chat.MessageType;
-import org._1mg.tt_backend.chat.dto.DieDTO;
-import org._1mg.tt_backend.chat.dto.JoinDTO;
-import org._1mg.tt_backend.chat.dto.LeaveDTO;
 import org._1mg.tt_backend.chat.service.ChatService;
 import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
-import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectedEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
@@ -42,112 +37,15 @@ public class WebSocketEventListener {
         log.info("WebSocket connected: sessionId={}, user={}", headerAccessor.getSessionId(), headerAccessor.getUser().getName());
     }
 
-    /**
-     * SUBSCRIBE 헤더 처리
-     * ENTER - 채팅방에 입장
-     * JOIN - 채팅방 가입
-     * 없음 - Chatroom List 구독, 채팅방 퇴장 시 재구독
-     */
+    //Subscribe 이벤트 발생 시 로그
     @EventListener
     public void handleSubscribeListener(SessionSubscribeEvent event) {
         log.info("subscribe event : {}", event.toString());
-
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String value = headerAccessor.getFirstNativeHeader("COMMAND");
-        if (value == null) {
-            log.info("Subscribe COMMAND IS NULL");
-            return;
-        }
-
-        MessageType messageType = MessageType.getMessageType(value.toUpperCase());
-        if (messageType == null) {
-            throw new RuntimeException("Unsupported message type: " + value);
-        }
-
-        switch (messageType) {
-            case ENTER -> {
-                log.info("Subscribe ENTER");
-            }
-            case JOIN -> {
-                log.info("Subscribe JOIN");
-                String profileId = headerAccessor.getFirstNativeHeader("profileId");
-                String chatroomId = headerAccessor.getFirstNativeHeader("chatroomId");
-                String nickname = headerAccessor.getFirstNativeHeader("nickname");
-                JoinDTO joinDTO = JoinDTO.builder()
-                        .profileId(profileId)
-                        .chatroomId(chatroomId)
-                        .build();
-
-                chatService.joinChatroom(joinDTO);
-                String joinMessage = nickname + "님이 입장하셨습니다";
-                messagingTemplate.convertAndSend("/sub/room/" + chatroomId, joinMessage);
-            }
-            default -> {
-                log.info("Subscribe UNKNOWN");
-            }
-        }
     }
 
-    /**
-     * UNSUBSCRIBE 헤더 처리
-     * LEAVE - 채팅방 퇴장
-     * DIE - 채팅방 탈퇴
-     * 업음 - 채팅방 입장 전에 잠깐 끊기
-     */
+    //Unsubscribe 이벤트 발생 시 로그
     @EventListener
     public void handleUnsubscribeListener(SessionUnsubscribeEvent event) {
         log.info("unsubscribe event : {}", event.toString());
-
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
-        String value = headerAccessor.getFirstNativeHeader("COMMAND");
-        if (value == null) {
-            log.info("Unsubscribe COMMAND IS NULL");
-            return;
-        }
-
-        MessageType messageType = MessageType.getMessageType(value.toUpperCase());
-        if (messageType == null) {
-            throw new RuntimeException("Unsupported message type: " + value);
-        }
-
-        switch (messageType) {
-            case LEAVE -> {
-                log.info("UnSubscribe LEAVE");
-                String profileId = headerAccessor.getFirstNativeHeader("profileId");
-                String chatroomId = headerAccessor.getFirstNativeHeader("chatroomId");
-                LeaveDTO leaveDTO = LeaveDTO.builder()
-                        .profileId(profileId)
-                        .chatroomId(chatroomId)
-                        .build();
-
-                chatService.leaveChatroom(leaveDTO);
-            }
-            case DIE -> {
-                log.info("UnSubscribe DIE");
-                String profileId = headerAccessor.getFirstNativeHeader("profileId");
-                String chatroomId = headerAccessor.getFirstNativeHeader("chatroomId");
-                DieDTO dieDTO = DieDTO.builder()
-                        .profileId(profileId)
-                        .chatroomId(chatroomId)
-                        .build();
-
-                chatService.dieChatroom(dieDTO);
-            }
-            default -> {
-                log.info("UnSubscribe UNKNOWN");
-            }
-        }
-    }
-
-    @EventListener
-    public void handleSendEvent(GenericMessage<?> message) {
-
-        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
-
-        log.info("================EVENT================");
-        log.info("header Accessor = {}", headerAccessor.getCommand());
-        log.info("header Accessor = {}", headerAccessor.getSessionId());
-        log.info("header Accessor = {}", headerAccessor.getUser());
-        log.info("payload = {}", message.getPayload());
     }
 }
