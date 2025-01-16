ae45bf5d2925b075baea3fd8fb064c089126ff31
whffu762 <whffu762@naver.com>
2025-01-16 16:31:36 +0900

    fix : @ModelAttribute로 파라미터 받아올라면 DTO에 @Setter 필수 없으면 매핑 못 함

--- src/main/java/org/_1mg/tt_backend/landmark/dto/LocationDTO.java
+++ src/main/java/org/_1mg/tt_backend/landmark/dto/LocationDTO.java
@@ -1,12 +1,12 @@
 package org._1mg.tt_backend.landmark.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
-@Builder
+@Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class LocationDTO {
