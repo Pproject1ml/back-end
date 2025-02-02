fae143412b10385ea5ef449badb7f048af5725b9
whffu762 <whffu762@naver.com>
2025-02-02 22:11:02 +0900

    cicd : secretFile 경로 설정 - 개발과 운영 환경에서 적용되는 yml 파일을 profile로 나눔 - firebase.admin.path : firebase 사용할 때 필요한 비공개 키 파일 - secret.yml : DB 정보 등 여러 비밀 정보가 포함된 yml파일 - 기존엔 docker-compose.yml 에서 적용했지만 내용이 점점 방대해지기 때문에 파일을 특정 경로에 두고 바인딩 마운트 하는 식으로 변경

--- src/main/java/org/_1mg/tt_backend/FCM/config/FirebaseConfig.java
+++ src/main/java/org/_1mg/tt_backend/FCM/config/FirebaseConfig.java
@@ -4,23 +4,40 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.util.StreamUtils;
 
 import javax.annotation.PostConstruct;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Configuration
 public class FirebaseConfig {
 
+    private final String firebasePath;
+
+    public FirebaseConfig(@Value("${firebase.admin.path}") String path) {
+        this.firebasePath = path;
+    }
+
     @PostConstruct
     public void initialize() {
+
+        log.info("Initializing Firebase");
         try {
-            InputStream serviceAccount = new ClassPathResource("firebase/chatloaction-firebase-adminsdk-fbsvc-f84c340e39.json").getInputStream();
+            InputStream serviceAccount = new FileInputStream(firebasePath);
+            log.info("finding file {}", firebasePath);
+
+            byte[] streamBytes = StreamUtils.copyToByteArray(serviceAccount);
+            log.info("service account {}", new String(streamBytes, StandardCharsets.UTF_8));
+
             FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .setCredentials(GoogleCredentials.fromStream(new ByteArrayInputStream(streamBytes)))
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
